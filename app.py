d3d71c18ec2638320a691476c86005cc7d7d82fd
kalpit sharma <kalpit.cool2006@gmail.com>
2025-04-10 22:35:09 +0530

    adding changes for vision

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify , render_template
+from flask import Flask, request, jsonify , render_template , Response
 from flask_cors import CORS
-from model.clip_emotion import detect_emotion
+from models.clip_emotion import detect_emotion
 import cv2
+from camera import VideoCamera
 import os
 
 app = Flask(__name__)
@@ -14,6 +15,25 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 def home():
     return render_template('index.html')
 
+
+@app.route('/vit')
+def index_vit():
+    return render_template('vit.html')
+
+@app.route('/cnnkeras')
+def index_cnn():
+    return render_template('cnnkeras.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
